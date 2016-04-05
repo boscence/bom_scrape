8bdad59034e70e0e86f36734341f1d2f4a577ef5
boscence <boscence@gmail.com>
2016-04-05 14:30:56 +0200

    Updated with libraries

--- bom_scrape.py
+++ bom_scrape.py
@@ -4,6 +4,8 @@ Created on Fri Mar 25 22:55:35 2016
 
 @author: ab
 """
+from lxml import html
+import requests
 
 bom = requests.get('http://www.bom.gov.au/vic/forecasts/melton.shtml')
 tree = html.fromstring(bom.content)
@@ -13,6 +15,5 @@ max = tree.xpath('//em[@class="max"]/text()')
 min = tree.xpath('//em[@class="min"]/text()')
 rain = tree.xpath('//em[@class="rain"]/text()')
 frain = tree.xpath('//dd[@class="rain"]/text()')
-
 
 print(fdate,max,min,rain,frain)
